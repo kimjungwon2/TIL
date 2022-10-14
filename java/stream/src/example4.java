b70feb906a092632cce4eb667e4f34acaac1b30c
kimjungwon2 <mac20010@naver.com>
2022-10-15 03:19:58 +0900

    peek로 디버깅

--- java/stream/src/example4.java
+++ java/stream/src/example4.java
@@ -17,7 +17,9 @@ public class example4 {
 
         fileStream.map(File::getName)   //Stream<File> -> Stream<String>
                 .filter(s->s.indexOf('.')!=-1)//확장자가 없는 것은 제외
+                .peek(s->System.out.printf("filename=%s%n",s))
                 .map(s->s.substring(s.indexOf('.')+1))// 확장자만 추출
+                .peek(s->System.out.printf("extension=%s%n",s))
                 .map(String::toUpperCase) // 대문자 변환
                 .distinct()
                 .forEach(System.out::print); //JAVABAKTXT
