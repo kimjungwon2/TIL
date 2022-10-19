3ee6123b18e4410695aec7d8959072cd2d3b3c0d
kimjungwon2 <mac20010@naver.com>
2022-10-19 16:24:12 +0900

    파이선 기본 문법 공부

---
+++ algorithm/파이썬 기초/ex1.py
@@ -0,0 +1,18 @@
+import time
+
+a = 7
+b = 3
+
+print(a/b)
+
+print(a % b)
+
+print(a//b)
+
+print(a**7)
+
+# 제곱근
+print(a**0.5)
+
+
+print(round(1232.23898, 3))
---
+++ algorithm/파이썬 기초/ex3.py
@@ -0,0 +1,11 @@
+m = 4
+n = 3
+
+array = [[0]*m for _ in range(n)]
+
+print(array)
+
+
+array[1][1]=5
+
+print(array)
