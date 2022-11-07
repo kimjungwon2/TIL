d6247d1351968f3a75850c8d3bd17e8d1c75b260
kimjungwon2 <mac20010@naver.com>
2022-11-07 20:30:41 +0900

    기본 해시 메서드

---
+++ algorithm/해시/ex1.py
@@ -0,0 +1,26 @@
+hashDict = {'ftp': 21, 'ssh': 22, 'stmp': 25, 'http': 80}
+
+print(hashDict.keys())
+print(hashDict.values())
+print(hashDict.items())
+print(hashDict.get('ftps'))
+
+del hashDict['stmp']
+print(hashDict)
+
+hass = list(hashDict.keys())
+print(hass)
+
+
+hashDict.setdefault('e')
+print(hashDict)
+
+
+hashDict.setdefault('fg', 50)
+print(hashDict)
+
+hashDict.update(ftp=50,g=30)
+print(hashDict)
+
+hashDict.clear()
+print(hashDict)
