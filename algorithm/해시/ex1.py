a247db13b602b57ac9b4697b8a8a3e2635656096
kimjungwon2 <mac20010@naver.com>
2022-11-07 21:29:45 +0900

    해시 활용2

--- algorithm/해시/ex1.py
+++ algorithm/해시/ex1.py
@@ -15,12 +15,14 @@ print(hass)
 hashDict.setdefault('e')
 print(hashDict)
 
-
 hashDict.setdefault('fg', 50)
 print(hashDict)
 
 hashDict.update(ftp=50,g=30)
 print(hashDict)
+
+print("ftp" in hashDict)
+print("철수" in hashDict)
 
 hashDict.clear()
 print(hashDict)
