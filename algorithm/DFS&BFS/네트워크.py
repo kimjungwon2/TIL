cdb0d65ff7fdc9f50118665b6d81bebe1f3988da
kimjungwon2 <mac20010@naver.com>
2022-11-15 23:03:41 +0900

    DFS&BFS 연습(4)

---
+++ algorithm/DFS&BFS/네트워크.py
@@ -0,0 +1,28 @@
+answer = 1
+
+
+def solution(n, computers):
+    global answer
+
+    visited = n*[False]
+    node = 0
+
+    while True:
+        dfs(computers, node, visited)
+
+        if False in visited:
+            node = visited.index(False)
+            answer += 1
+        else:
+            break
+
+    return answer
+
+
+def dfs(computers, node, visited):
+    global answer
+    visited[node] = True
+
+    for i, neighbor in enumerate(computers[node]):
+        if (neighbor == 1 and visited[i] == False):
+            dfs(computers, i, visited)
