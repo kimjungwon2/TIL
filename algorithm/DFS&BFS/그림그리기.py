4d3896889edf8a2cf3dac8b954ba2ba3e6fcd639
kimjungwon2 <mac20010@naver.com>
2022-11-15 19:55:03 +0900

    BFS 연습(3)

--- algorithm/DFS&BFS/그림그리기.py
+++ algorithm/DFS&BFS/그림그리기.py
@@ -14,48 +14,48 @@ from collections import deque
 
 input = sys.stdin.readline
 n, m = map(int, input().split())
-
 graph = []
 
 for _ in range(n):
     graph.append(list(map(int, input().split())))
 
+visited = [[False]*m for _ in range(n)]
 
 
-visited = [[False] * m for _ in range(n)]
+def bfs(x, y):
+    count = 1
+    # 상,하,좌,우
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    queue = deque()
+    queue.append((x, y))
 
+    while queue:
+        x, y = queue.popleft()
 
-#상,하,좌,우
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
 
-
-def bfs(x,y):
-    result = 1
-    q = deque()
-    q.append((x,y))
-    while q:
-        ex,ey = q.popleft()
-        for k in range(4):
-            nx = ex + dx[k]
-            ny = ey + dy[k]
-
-            if 0 <= ny < m and 0 <= nx < n:
-                if graph[nx][ny] == 1 and visited[nx][ny] == False:
-                    result += 1
+            if (0 <= nx < n and 0 <= ny < m):
+                if (graph[nx][ny] == 1 and visited[nx][ny] == False):
                     visited[nx][ny] = True
-                    q.append((nx,ny))
-    return result
+                    queue.append((nx, ny))
+                    count += 1
+
+    return count
 
 
-count = 0
+cnt = 0
 max_v = 0
+
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 1 and visited[i][j] == False:
+        if (visited[i][j] == False and graph[i][j] == 1):
             visited[i][j] = True
-            count += 1
+            cnt += 1
             max_v = max(max_v, bfs(i, j))
 
-print(count)
+
+print(cnt)
 print(max_v)
--- algorithm/문자열/슬라이스.py
+++ algorithm/문자열/슬라이스.py
@@ -5,3 +5,5 @@ print(a[-2:])
 print(a[1:4])
 
 print(a[:-1])
+
+print(a[1:3])
