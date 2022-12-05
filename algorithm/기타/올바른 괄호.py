463d99356ff260fa68bfa7787a2ab98fe84fcebd
kimjungwon2 <mac20010@naver.com>
2022-12-05 22:48:05 +0900

    스택

---
+++ algorithm/기타/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = True
+    stack = []
+
+    for i in range(len(s)):
+        if (s[i] == '('):
+            stack.append('(')
+        else:
+            if (len(stack) == 0):
+                return False
+            stack.pop()
+
+    if (len(stack) != 0):
+        return False
+
+    return True
