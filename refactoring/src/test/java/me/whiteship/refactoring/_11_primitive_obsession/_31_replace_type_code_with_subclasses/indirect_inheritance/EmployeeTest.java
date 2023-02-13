b99222af9bfd4bf7ab131e57c5332641edb10610
kimjungwon2 <mac20010@naver.com>
2023-02-13 18:41:54 +0900

    리팩토링 - 기본형 집착

--- refactoring/src/test/java/me/whiteship/refactoring/_11_primitive_obsession/_31_replace_type_code_with_subclasses/indirect_inheritance/EmployeeTest.java
+++ refactoring/src/test/java/me/whiteship/refactoring/_11_primitive_obsession/_31_replace_type_code_with_subclasses/indirect_inheritance/EmployeeTest.java
@@ -1,10 +1,12 @@
 package me.whiteship.refactoring._11_primitive_obsession._31_replace_type_code_with_subclasses.indirect_inheritance;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class EmployeeTest {
+
 
     @Test
     void capitalizedType() {
