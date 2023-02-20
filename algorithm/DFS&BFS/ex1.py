51ce6b0256891a07a3c85f2130c35ef33b6f7230
kimjungwon2 <mac20010@naver.com>
2023-02-20 20:05:00 +0900

    bfs

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,22 +1,46 @@
 from collections import deque
+import sys
 
-graph = {
-    'A':['B','D','E'],
-    'B':['A','C','D'],
-    'C':['B'],
-    'D':['A','B'],
-    'E':['A']
-}
+input =sys.stdin.readline
 
-def bfs(graph,start_v):
-    queue = deque(start_v)
-    visited=[start_v]
+n, m = map(int,input().split())
+
+picture = [ list(map(int, input().split())) for _ in range(n)]
+visited = [m * [False] for _ in range(n)]
+
+
+count = 0
+maxv = 0 
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    size = 1
+
+    dx = [-1,1,0,0 ]
+    dy = [0,0,1,-1]
 
     while queue:
-        current = queue.popleft()
-        for next_v in graph[current]:
-            if next_v not in visited:
-                queue.append(next_v)
-                visited.append(next_v)
+        x,y = queue.popleft()
 
-    return visited
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+            if(0<=nx<n and 0<=ny<m):
+                if(picture[nx][ny]==1 and visited[nx][ny]==False):
+                    visited[nx][ny]=True
+                    queue.append((nx,ny))
+                    size+=1
+
+    return size
+
+for i in range(n):
+    for j in range(m):
+        if(visited[i][j]==False and picture[i][j]==1):
+            visited[i][j]=True
+            maxv = max(maxv, bfs(i,j))
+            count+=1
+
+print(count)
+print(maxv)
