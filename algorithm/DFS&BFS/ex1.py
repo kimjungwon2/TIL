23ff83e7716a56685ad705eb4aab8e1a464a09d1
kimjungwon2 <mac20010@naver.com>
2023-03-06 22:29:45 +0900

    소수 찾기

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,19 +1,53 @@
-import sys
-input = sys.stdin.readline
+import math
 
-N,K = map(int, input().split())
-nums = list(map(int,input().split()))
-each = 0
-
-for i in range(K):
-    each+=nums[i]
-
-maxv = each
-
-for i in range(K,N):
-    each+= nums[i]
-    each-= nums[i-K]
-    maxv = max(maxv,each)
+def solution(numbers):
+    answer = 0
+    arr = [i for i in numbers]
+    visited = [False]*(len(numbers)+1)
+    final = []
+      
+    recur(0,arr,visited,'',final)
+    
+    answer = len(set(final))
+    
+    return answer
 
 
-print(maxv)
+def recur(num,arr,visited,result,final):  
+    length = len(arr)
+    if(num==length):
+        return
+    
+    for i in range(1,length+1):
+        if visited[i] == False:
+            visited[i] = True
+            
+            result+=arr[i-1]
+
+            #소수 확인
+            a = isPrime(int(result))
+            if(a==True):
+                final.append(int(result))
+            else:
+                a = False
+            
+            recur(num+1,arr,visited,result,final)
+            
+            #다시 올라가기
+            visited[i] = False
+            #문자열 자르기
+            result = result[:-1]
+            
+            
+                  
+def isPrime(numbers):
+    number = int(numbers)
+    
+    if(number<=1):
+        return False
+    
+    for i in range(2,int(math.sqrt(number))+1):
+        if number%i ==0:
+            return False
+        
+    return True
