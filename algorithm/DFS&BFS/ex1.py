2c5e62f12a5d72a495c2f2512e098c9909cde4b5
kimjungwon2 <mac20010@naver.com>
2023-03-08 17:38:57 +0900

    bfs

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,40 +1,38 @@
 from collections import deque
 
-def bfs(node, tree, visited, wire, cnt):
+
+def solution(maps):
+    answer = 0
+
+    answer = bfs(maps)
+
+    if (answer == 1):
+        return -1
+    else:
+        return answer
+
+
+def bfs(maps):
+    n_x = len(maps)
+    n_y = len(maps[0])
+
     queue = deque()
-    queue.append([node, tree, visited, wire])
-    visited[node] = True
+    queue.append((0, 0))
+
+    #상,하,좌,우
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
     while queue:
-        node, tree, visited, wire = queue.popleft()
-        cnt += 1
+        x, y = queue.popleft()
 
-        for i in tree[node]:
-            if not ((node == wire[0] and i == wire[1]) or (node == wire[1] and i == wire[0])):
-                if not visited[i]:
-                    visited[i] = True
-                    queue.append([i, tree, visited, wire])
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y+dy[i]
 
-    return cnt
+            if (0 <= nx < n_x and 0 <= ny < n_y):
+                if (maps[nx][ny] == 1):
+                    maps[nx][ny] = maps[x][y]+1
+                    queue.append((nx, ny))
 
-
-def solution(n, wires):
-    answer = 1e9
-    tree = [[] for _ in range(n + 1)]
-
-    for wire in wires:
-        a, b = wire
-        tree[a].append(b)
-        tree[b].append(a)
-
-    for wire in wires:
-        visited = [False] * (n + 1)
-        temp = []
-        for i in range(1, n + 1):
-            if not visited[i]:
-                cnt = bfs(i, tree, visited, wire, 0)
-                temp.append(cnt)
-
-        answer = min(answer, abs(temp[0] - temp[1]))
-
-    return answer
+    return maps[n_x-1][n_y-1]
