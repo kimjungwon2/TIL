8c197a7b4ae03ae0afa47f175ebe12c66781b032
kimjungwon2 <mac20010@naver.com>
2023-03-12 17:56:43 +0900

    스택

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,38 +1,22 @@
-from collections import deque
-
-
-def solution(maps):
+def solution(priorities, location):
     answer = 0
+    arr = []
+    length = len(priorities)
 
-    answer = bfs(maps)
+    while (True):
+        max_value = max(priorities)
 
-    if (answer == 1):
-        return -1
-    else:
-        return answer
+        for i, j in enumerate(priorities):
+            if (max_value == j and max_value != 0):
+                arr.append((j, i))
+                priorities[i] = 0
+                max_value = max(priorities)
 
+        if (len(arr) == length):
+            break
 
-def bfs(maps):
-    n_x = len(maps)
-    n_y = len(maps[0])
+    for i, j in enumerate(arr):
+        if (j[1] == location):
+            answer = i+1
 
-    queue = deque()
-    queue.append((0, 0))
-
-    #상,하,좌,우
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y+dy[i]
-
-            if (0 <= nx < n_x and 0 <= ny < n_y):
-                if (maps[nx][ny] == 1):
-                    maps[nx][ny] = maps[x][y]+1
-                    queue.append((nx, ny))
-
-    return maps[n_x-1][n_y-1]
+    return answer
