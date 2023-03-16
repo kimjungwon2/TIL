93dc970de2332952b41c45b5c26f1998c4631204
kimjungwon2 <mac20010@naver.com>
2023-03-16 19:32:37 +0900

    스택

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,32 +1,16 @@
-def solution(genres, plays):
-    answer = []
-    album = dict()
-    album_sum = dict()
+def solution(s):
+    answer = True
+    stack = []
 
-    for index, (i, j) in enumerate(zip(genres, plays)):
-        if (i not in album):
-            album[i] = [(index, j)]
-            album_sum[i] = j
+    for i in range(len(s)):
+        if (s[i] == '('):
+            stack.append('(')
         else:
-            album[i].append((index, j))
-            album_sum[i] += j
+            if (len(stack) == 0):
+                return False
+            stack.pop()
 
-    for i in album:
-        album[i] = sorted(album[i], key=lambda x: x[1], reverse=True)
+    if (len(stack) != 0):
+        return False
 
-    max_value = []
-
-    for i, j in zip(album_sum.keys(), album_sum.values()):
-        max_value.append((i, j))
-
-    max_value = sorted(max_value, key=lambda x: x[1], reverse=True)
-
-    for i in max_value:
-        for j in range(2):
-            if (len(album[i[0]]) == 1):
-                answer.append(album[i[0]][j][0])
-                break
-            else:
-                answer.append(album[i[0]][j][0])
-
-    return answer
+    return True
