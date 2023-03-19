728df2a066899119fb455ba104e993f273b8ce8b
kimjungwon2 <mac20010@naver.com>
2023-03-19 20:59:50 +0900

    해시

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,10 +1,32 @@
-from itertools import permutations
+def solution(genres, plays):
+    answer = []
+    album = dict()
+    album_sum = dict()
 
+    for index, (i, j) in enumerate(zip(genres, plays)):
+        if (i not in album):
+            album[i] = [(index, j)]
+            album_sum[i] = j
+        else:
+            album[i].append((index, j))
+            album_sum[i] += j
 
-def solution(numbers):
-    str_numbers = list(map(str, numbers))
-    str_numbers.sort(key=lambda x: x*3, reverse=True)
+    for i in album:
+        album[i] = sorted(album[i], key=lambda x: x[1], reverse=True)
 
-    answer = str(int(''.join(str_numbers)))
+    max_value = []
+
+    for i, j in zip(album_sum.keys(), album_sum.values()):
+        max_value.append((i, j))
+
+    max_value = sorted(max_value, key=lambda x: x[1], reverse=True)
+
+    for i in max_value:
+        for j in range(2):
+            if (len(album[i[0]]) == 1):
+                answer.append(album[i[0]][j][0])
+                break
+            else:
+                answer.append(album[i[0]][j][0])
 
     return answer
