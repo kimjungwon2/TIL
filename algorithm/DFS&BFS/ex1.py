0ff0bb2c5f694ae244750e67aca2c87674bc2a7d
kimjungwon2 <mac20010@naver.com>
2023-03-20 15:15:27 +0900

    해시

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,32 +1,17 @@
-def solution(genres, plays):
-    answer = []
-    album = dict()
-    album_sum = dict()
+from collections import Counter
 
-    for index, (i, j) in enumerate(zip(genres, plays)):
-        if (i not in album):
-            album[i] = [(index, j)]
-            album_sum[i] = j
-        else:
-            album[i].append((index, j))
-            album_sum[i] += j
 
-    for i in album:
-        album[i] = sorted(album[i], key=lambda x: x[1], reverse=True)
+def solution(nums):
 
-    max_value = []
+    answer = 0
 
-    for i, j in zip(album_sum.keys(), album_sum.values()):
-        max_value.append((i, j))
+    length = len(nums)/2
+    count = list(Counter(nums))
+    count_num = len(count)
 
-    max_value = sorted(max_value, key=lambda x: x[1], reverse=True)
-
-    for i in max_value:
-        for j in range(2):
-            if (len(album[i[0]]) == 1):
-                answer.append(album[i[0]][j][0])
-                break
-            else:
-                answer.append(album[i[0]][j][0])
+    if (length <= count_num):
+        answer = length
+    else:
+        answer = count_num
 
     return answer
