48d6e344320b34ce2c87f08d97460ae67afbf790
kimjungwon2 <mac20010@naver.com>
2023-03-21 18:07:28 +0900

    bfs

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,17 +1,38 @@
-from collections import Counter
+from collections import deque
 
 
-def solution(nums):
-
+def solution(maps):
     answer = 0
 
-    length = len(nums)/2
-    count = list(Counter(nums))
-    count_num = len(count)
+    answer = bfs(maps)
 
-    if (length <= count_num):
-        answer = length
+    if (answer == 1):
+        return -1
     else:
-        answer = count_num
+        return answer
 
-    return answer
+
+def bfs(maps):
+    n_x = len(maps)
+    n_y = len(maps[0])
+
+    queue = deque()
+    queue.append((0, 0))
+
+    #상,하,좌,우
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y+dy[i]
+
+            if (0 <= nx < n_x and 0 <= ny < n_y):
+                if (maps[nx][ny] == 1):
+                    maps[nx][ny] = maps[x][y]+1
+                    queue.append((nx, ny))
+
+    return maps[n_x-1][n_y-1]
