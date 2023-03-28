af08ff24269e791caf83e9cefd979ecd7733afb3
kimjungwon2 <mac20010@naver.com>
2023-03-28 18:19:32 +0900

    bfs

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,45 +1,38 @@
 from collections import deque
 
 
-def solution(rectangle, characterX, characterY, itemX, itemY):
-    answer = 0
-
-    field = [[-1] * 102 for i in range(102)]
-
-
-    for r in rectangle:
-
-        x1, y1, x2, y2 = map(lambda x: x*2, r)
-
-        for i in range(x1, x2+1):
-            for j in range(y1, y2+1):
-            	
-                if x1 < i < x2 and y1 < j < y2:
-                    field[i][j] = 0
-
-                elif field[i][j] != 0:
-                    field[i][j] = 1
-
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-
-    q = deque()
-    q.append([characterX * 2, characterY * 2])
-    visited = [[1] * 102 for i in range(102)]
-
-    while q:
-        x, y = q.popleft()
-
-        if x == itemX * 2 and y == itemY * 2:
-            answer = visited[x][y] // 2
-            break
-
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-
-            if field[nx][ny] == 1 and visited[nx][ny] == 1:
-                q.append([nx, ny])
-                visited[nx][ny] = visited[x][y] + 1
-
+def solution(begin, target, words):
+    answer = bfs(begin, target, words)
     return answer
+
+
+def bfs(begin, target, words):
+    count = 0
+    queue = deque()
+    queue.append((begin, count))
+
+    if (target not in words):
+        return 0
+
+    while queue:
+        start, count = queue.popleft()
+
+        for i in words:
+            if (change(start, i) == True):
+                if i == target:
+                    return count+1
+                else:
+                    queue.append((i, count+1))
+
+
+def change(word, change):
+    length = len(word)
+    diff = 0
+    for i in range(length):
+        if word[i] != change[i]:
+            diff += 1
+
+    if diff == 1:
+        return True
+    else:
+        return False
