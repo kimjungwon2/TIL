a259699f92c7dc49ced375db10904a97971dd009
kimjungwon2 <mac20010@naver.com>
2023-04-02 17:04:36 +0900

    완전 탐색

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,29 +1,35 @@
-from collections import deque
+ans = 0
 
-
-def solution(n, computers):
+def solution(k, dungeons):
     answer = 0
+    length = len(dungeons)
+    visited = [False] * length
 
-    visited = [False]*(n+1)
+    recur(k, dungeons, length, visited, answer)
 
-    for i in range(n):
-        if (visited[i] == False):
-            bfs(i, computers, visited)
-            answer += 1
+    return ans
 
-    return answer
+def recur(k, dungeons, length, visited, answer):
+    global ans
+    ans = max(ans, answer)
 
+    min_value = dungeons[0][0]
 
-def bfs(node, computers, visited):
-    queue = deque()
-    queue.append(node)
+    for i in dungeons:
+        min_value = min(min_value,i[0])
 
-    while queue:
-        node = queue.popleft()
+    if(k<min_value):
+        return
 
-        for index, i in enumerate(computers[node]):
-            if (visited[index] == False and i == 1):
-                visited[index] = True
-                queue.append(index)
+    for i in range(length):
+        if(k>=dungeons[i][0] and visited[i]==False):
+            visited[i]=True
+            k-=dungeons[i][1]
+            answer+=1
 
-    return 0
+            recur(k, dungeons, length, visited, answer)
+
+            #다시 위로 올라가기
+            visited[i]=False
+            k+=dungeons[i][1]
+            answer-=1
