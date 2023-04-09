5454bd30d1ed750c49149f480ccbe0ab7ed58440
kimjungwon2 <mac20010@naver.com>
2023-04-09 21:08:42 +0900

    그리드

--- algorithm/그리드/체육복.py
+++ algorithm/그리드/체육복.py
@@ -1,11 +1,11 @@
 def solution(n, lost, reserve):
-    _reserve = [r for r in reserve if r not in lost]
-    _lost = [l for l in lost if l not in reserve]
-    for r in _reserve:
+    reserve = [r for r in reserve if r not in lost]
+    lost = [l for l in lost if l not in reserve]
+    for r in reserve:
         f = r - 1
         b = r + 1
-        if f in _lost:
-            _lost.remove(f)
-        elif b in _lost:
-            _lost.remove(b)
-    return n - len(_lost)
+        if f in lost:
+            lost.remove(f)
+        elif b in lost:
+            lost.remove(b)
+    return n - len(lost)
