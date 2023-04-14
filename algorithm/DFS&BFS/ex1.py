ee0c8ef7992167accf292bdbac36ea1e270b69c0
kimjungwon2 <mac20010@naver.com>
2023-04-15 00:01:29 +0900

    bfs 연습(2)

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,23 +1,47 @@
-answer = 0
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+picture = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[False]*m for _ in range(n)]
 
 
-def solution(numbers, target):
+def bfs(x, y):
+    queue = deque()
+    size = 1
+    queue.append((x,y))
 
-    length = len(numbers)
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, 1, -1]
 
-    recur(numbers, target, 0, 0, length)
+    while queue:
+        x, y = queue.popleft()
 
-    return answer
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+
+            if (0 <= nx < n and 0 <= ny < m):
+                if (visited[nx][ny] == False and picture[nx][ny] == 1):
+                    visited[nx][ny] = True
+                    size += 1
+                    queue.append((nx, ny))
+
+    return size
 
 
-def recur(numbers, result, target, n, length):
-    global answer
-    if (length == n):
-        if (result == target):
-            answer += 1
-            return
-        else:
-            return
+max_size = 0
+count = 0
 
-    recur(numbers, result+numbers[n], target, n+1, length)
-    recur(numbers, result-numbers[n], target, n+1, length)
+for i in range(n):
+    for j in range(m):
+        if (visited[i][j] == False and picture[i][j] == 1):
+            visited[i][j] = True
+            max_size = max(bfs(i, j), max_size)
+            count += 1
+
+print(count)
+print(max_size)
