f6d02c624f8b4a3b32ea1719b231d8083d745902
kimjungwon2 <mac20010@naver.com>
2023-04-24 17:08:08 +0900

    bfs 연습2

--- algorithm/DFS&BFS/ex1.py
+++ algorithm/DFS&BFS/ex1.py
@@ -1,29 +1,43 @@
 from collections import deque
 
-graph = {
-    'A': ['B', 'D', 'E'],
-    'B': ['A', 'C', 'D'],
-    'C': ['B'],
-    'D': ['A', 'B'],
-    'E': ['A']
-}
+
+def solution(maps):
+    answer = 0
+
+    answer = bfs(0, 0, maps)
+
+    if (answer == 1):
+        return -1
+
+    return answer
 
 
-def bfs(graph, start):
-    queue = deque(start)
-    visited = [start]
+def bfs(x, y, maps):
+    n = len(maps)
+    m = len(maps[0])
+
+    queue = deque()
+    queue.append((x, y))
+
+    #상,하,좌,우
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
     while queue:
-        current = queue.popleft()
+        x, y = queue.popleft()
 
-        for next_v in graph[current]:
-            if next_v not in visited:
-                queue.append(next_v)
-                visited.append(next_v)
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
 
-    return visited
+            if (nx < 0 or ny < 0 or nx >= n or ny >= m):
+                continue
 
+            if (maps[nx][ny] == 0):
+                continue
 
-bfs(graph, 'A')
+            if (maps[nx][ny] == 1):
+                maps[nx][ny] = maps[x][y]+1
+                queue.append((nx, ny))
 
-print(bfs(graph, 'A'))
+    return maps[n-1][m-1]
