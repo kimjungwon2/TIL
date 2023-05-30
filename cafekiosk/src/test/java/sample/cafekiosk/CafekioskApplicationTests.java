06301165e764a242143088a03d0d9adf3c5adc36
kimjungwon2 <mac20010@naver.com>
2023-05-31 01:12:57 +0900

    TEST: 경계값 테스트

---
+++ cafekiosk/src/test/java/sample/cafekiosk/CafekioskApplicationTests.java
@@ -0,0 +1,79 @@
+package sample.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import sample.cafekiosk.unit.CafeKiosk;
+import sample.cafekiosk.unit.beverage.Americano;
+import sample.cafekiosk.unit.beverage.Latte;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class CafekioskApplicationTests {
+
+    @Test
+    void add_manual() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        System.out.println(">>> 담긴 음료 수 : "+cafeKiosk.getBeverages().size());
+        System.out.println(">>> 담긴 음료 : "+cafeKiosk.getBeverages().get(0).getName());
+    }
+
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void addServeral() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano,2);
+
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+    }
+
+    @Test
+    void addZeroBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        assertThatThrownBy(()->cafeKiosk.add(americano,0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음료는 1잔 이상 주문하실 수 있습니다.");
+    }
+
+    @Test
+    void remove() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+
+        cafeKiosk.remove(americano);
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(0);
+        
+    }
+
+    @Test
+    void clear() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(2);
+
+        cafeKiosk.clear();
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(0);
+
+    }
+
+}
