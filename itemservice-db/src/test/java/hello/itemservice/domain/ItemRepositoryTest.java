456e9fee56e2bdecda0b32766050446921a45f09
jungwon <mac2001035@gmail.com>
2023-09-26 19:12:50 +0900

    TEST: Item Repository 테스트

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -52,10 +52,8 @@ class ItemRepositoryTest {
     void save() {
         //given
         Item item = new Item("itemA", 10000, 10);
-
         //when
         Item savedItem = itemRepository.save(item);
-
         //then
         Item findItem = itemRepository.findById(item.getId()).get();
         assertThat(findItem).isEqualTo(savedItem);
@@ -67,15 +65,15 @@ class ItemRepositoryTest {
         Item item = new Item("item1", 10000, 10);
         Item savedItem = itemRepository.save(item);
         Long itemId = savedItem.getId();
-
         //when
         ItemUpdateDto updateParam = new ItemUpdateDto("item2", 20000, 30);
         itemRepository.update(itemId, updateParam);
-
         //then
         Item findItem = itemRepository.findById(itemId).get();
+
         assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
         assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+
         assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
     }
 
@@ -85,30 +83,17 @@ class ItemRepositoryTest {
         Item item1 = new Item("itemA-1", 10000, 10);
         Item item2 = new Item("itemA-2", 20000, 20);
         Item item3 = new Item("itemB-1", 30000, 30);
-
-        log.info("repository={}", itemRepository.getClass());
         itemRepository.save(item1);
         itemRepository.save(item2);
         itemRepository.save(item3);
-
-        //둘 다 없음 검증
+        //여기서 3개 이상이 조회되는 문제가 발생
         test(null, null, item1, item2, item3);
-        test("", null, item1, item2, item3);
-
-        //itemName 검증
-        test("itemA", null, item1, item2);
-        test("temA", null, item1, item2);
-        test("itemB", null, item3);
-
-        //maxPrice 검증
-        test(null, 10000, item1);
-
-        //둘 다 있음 검증
-        test("itemA", 10000, item1);
     }
 
     void test(String itemName, Integer maxPrice, Item... items) {
-        List<Item> result = itemRepository.findAll(new ItemSearchCond(itemName, maxPrice));
+        List<Item> result = itemRepository.findAll(new ItemSearchCond(itemName,
+                maxPrice));
         assertThat(result).containsExactly(items);
     }
 }
+
