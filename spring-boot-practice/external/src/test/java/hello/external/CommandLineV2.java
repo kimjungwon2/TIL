e3faa11993d7518ca30f07f4ee62a660be81fc7d
kimjungwon2 <mac20010@naver.com>
2023-10-26 21:21:37 +0900

    FEAT: 커맨드 라인 옵션 인수(2)

--- spring-boot-practice/external/src/test/java/hello/external/CommandLineV2.java
+++ spring-boot-practice/external/src/test/java/hello/external/CommandLineV2.java
@@ -1,6 +1,7 @@
 package hello.external;
 
 import java.util.List;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.DefaultApplicationArguments;
@@ -15,7 +16,23 @@ public class CommandLineV2 {
 
         ApplicationArguments appArgs = new DefaultApplicationArguments();
         log.info("SourceArgs = {}", List.of(appArgs.getSourceArgs()));
+        log.info("NonOptionsArgs = {}", appArgs.getNonOptionArgs());
+        log.info("OptionsNames = {}", appArgs.getOptionNames());
 
+        Set<String> optionNames = appArgs.getOptionNames();
 
+        for(String optionName : optionNames){
+            log.info("option arg {}={}",optionName,appArgs.getOptionValues(optionName));
+        }
+
+        List<String> url = appArgs.getOptionValues("url");
+        List<String> username = appArgs.getOptionValues("username");
+        List<String> password = appArgs.getOptionValues("password");
+        List<String> mode = appArgs.getOptionValues("mode");
+
+        log.info("url={}",url);
+        log.info("username={}", username);
+        log.info("password={}", password);
+        log.info("mode={}", mode);
     }
 }
