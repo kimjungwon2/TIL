dfa607fdef8bf76a1b8f0bff8c982c4be21f8204
kimjungwon2 <mac20010@naver.com>
2023-10-27 22:06:53 +0900

    FEAT: 커맨드 라인 옵션 인수와 스프링 부트(2)

--- spring-boot-practice/external/src/main/java/hello/CommandLineBean.java
+++ spring-boot-practice/external/src/main/java/hello/CommandLineBean.java
@@ -1,5 +1,8 @@
 package hello;
 
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,5 +15,16 @@ public class CommandLineBean {
 
     public CommandLineBean(ApplicationArguments arguments) {
         this.arguments = arguments;
+    }
+
+    @PostConstruct
+    public void init(){
+        log.info("source {}", List.of(arguments.getSourceArgs()));
+        log.info("optionNames {}", arguments.getOptionNames());
+        Set<String> optionNames = arguments.getOptionNames();
+
+        for(String optionName: optionNames){
+            log.info("option args {}={}", optionName, arguments.getOptionValues(optionName));
+        }
     }
 }
