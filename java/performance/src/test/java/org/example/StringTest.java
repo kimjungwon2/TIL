98f03f86d9d1b414beb17312a3022d55a2e3ab2b
kimjungwon2 <mac20010@naver.com>
2023-11-01 20:33:36 +0900

    FEAT: String, StringBuilder, StringBuffer 성능 비교.

--- java/performance/src/test/java/org/example/StringTest.java
+++ java/performance/src/test/java/org/example/StringTest.java
@@ -1,11 +1,51 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 
 class StringTest {
+    private static final String aValue="abcde";
+
 
     public static void main(String[] args) {
+
+
+            String a = new String();
+            StringBuffer b = new StringBuffer();
+            StringBuilder c = new StringBuilder();
+
+            Long beforeTime = System.currentTimeMillis();
+
+            for (int loop = 0; loop < 1000000; loop++) {
+                a += aValue;
+            }
+
+            Long afterTime = System.currentTimeMillis();
+            Long secDiffTime = (afterTime - beforeTime)/1000;
+
+            System.out.println("String 연산 시간"+secDiffTime);
+
+
+            Long beforeTime2 = System.currentTimeMillis();
+
+            for (int loop = 0; loop < 1000000; loop++) {
+                b.append(aValue);
+            }
+
+            Long afterTime2 = System.currentTimeMillis();
+            Long secDiffTime2 = (afterTime2 - beforeTime2)/1000;
+
+            System.out.println("StringBuffer 연산 시간"+secDiffTime2);
+
+
+
+            Long beforeTime3 = System.currentTimeMillis();
+            for (int loop = 0; loop < 1000000; loop++) {
+                c.append(aValue);
+            }
+            Long afterTime3 = System.currentTimeMillis();
+            Long secDiffTime3 = (afterTime3 - beforeTime3)/1000;
+
+            System.out.println("StringBuilder 연산 시간"+secDiffTime3);
 
     }
 }
