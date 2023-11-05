dfbf943a7cf38be1b2efed8c1f3ab63539514073
kimjungwon2 <mac20010@naver.com>
2023-11-05 19:34:00 +0900

    FEAT:  static 변수

---
+++ java/performance/src/main/java/org/example/variable/StaticBasicTypes.java
@@ -0,0 +1,12 @@
+package org.example.variable;
+
+
+public class StaticBasicTypes {
+
+    int Variable;
+    static int classVariable;
+
+    public void method(int parmameter){
+        int localVariable;
+    }
+}
