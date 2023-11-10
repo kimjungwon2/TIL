010585badf5e97201c3f64e5d3de3fc3d658941a
kimjungwon2 <mac20010@naver.com>
2023-11-10 18:37:44 +0900

    FEAT: Synchronized 예제

---
+++ java/performance/src/main/java/org/example/thread/Sleep.java
@@ -0,0 +1,30 @@
+package org.example.thread;
+
+public class Sleep extends Thread{
+    public void run(){
+        try{
+            Thread.sleep(10000);
+        }catch(InterruptedException e){
+            System.out.println("Somebody stopped me T T");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String args[]){
+        Sleep s = new Sleep();
+        s.start();
+        try{
+            int cnt =0;
+            while(cnt<5){
+                s.join(1000);
+                cnt++;
+                System.out.format("%d second waited\n", cnt);
+            }
+            if(s.isAlive()){
+                s.interrupt();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
