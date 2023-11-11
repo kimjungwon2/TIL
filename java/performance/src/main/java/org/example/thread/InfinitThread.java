ebee48205be7de5f8f3036cb5c38c31a26919c4d
kimjungwon2 <mac20010@naver.com>
2023-11-11 19:53:00 +0900

    FEAT: Interrupt 예제

---
+++ java/performance/src/main/java/org/example/thread/InfinitThread.java
@@ -0,0 +1,17 @@
+package org.example.thread;
+
+public class InfinitThread extends Thread{
+
+    int value = Integer.MIN_VALUE;
+    private boolean flag = true;
+
+    public void run(){
+        while(flag){
+            value++;
+            if(value == Integer.MAX_VALUE){
+                value = Integer.MIN_VALUE;
+                System.out.println("MAX_VALUE reached !!! ");
+            }
+        }
+    }
+}
---
+++ java/performance/src/main/java/org/example/thread/InterruptSample.java
@@ -0,0 +1,14 @@
+package org.example.thread;
+
+public class InterruptSample {
+
+    public static void main(String[] args) throws Exception {
+        InfinitThread infinit = new InfinitThread();
+        infinit.start();
+        Thread.sleep(2000);
+        System.out.println("isInterrupted=" + infinit.isInterrupted());
+        infinit.interrupt();
+        System.out.println("isInterrupted="+infinit.isInterrupted());
+    }
+
+}
