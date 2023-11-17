c66091220977fb023f29a114358be4315f20b470
kimjungwon2 <mac20010@naver.com>
2023-11-17 18:07:50 +0900

    FEAT: System Format 사용

---
+++ java/performance/src/main/java/org/example/log/FormatSample.java
@@ -0,0 +1,8 @@
+package org.example.log;
+
+public class FormatSample {
+
+    public static void main(String[] args) {
+        System.out.format("Name=%s long value=%d floatvalue=%f\n","Format",1,1.5);
+    }
+}
