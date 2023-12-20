21e4175ae1c04c90f19e6139f248d6067a52e832
kimjungwon2 <mac20010@naver.com>
2023-12-21 01:03:19 +0900

    FEAT: 예외와 트랜잭션 커밋, 롤백 - 활용.

---
+++ spring-db-utilization/springtx/src/main/java/com/example/springtx/order/Order.java
@@ -0,0 +1,21 @@
+package com.example.springtx.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="orders")
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username; // 정상, 예외, 잔고부족
+    private String payStatus; //대기, 완료
+
+}
