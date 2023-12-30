3b46e1e11642ec080c6f3e9518e8a480bc457bd7
kimjungwon2 <mac20010@naver.com>
2023-12-30 17:50:59 +0900

    FEAT: OS 환경 변수.

--- spring-boot-practice/external/src/test/java/hello/external/OsEnvTest.java
+++ spring-boot-practice/external/src/test/java/hello/external/OsEnvTest.java
@@ -2,6 +2,16 @@ package hello.external;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 class OsEnvTest {
 
+    public static void main(String[] args){
+        Map<String, String> envMap = System.getenv();
+        for (String key : envMap.keySet()) {
+            log.info("env {}={}",key,System.getenv(key));
+        }
+    }
 }
