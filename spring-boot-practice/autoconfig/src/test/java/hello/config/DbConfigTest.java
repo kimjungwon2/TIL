0643673563ccf36f6ae82654b5cf37d5b867243f
kimjungwon2 <mac20010@naver.com>
2023-12-31 17:00:17 +0900

    FEAT: 스프링 부트의 자동 구성.

---
+++ spring-boot-practice/autoconfig/src/test/java/hello/config/DbConfigTest.java
@@ -0,0 +1,45 @@
+package hello.config;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.TransactionManager;
+
+@Slf4j
+@SpringBootTest
+class DbConfigTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    TransactionManager transactionManager;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @DisplayName("")
+    @Test
+    void checkBean(){
+      //given
+        log.info("dataSource= {}",dataSource);
+        log.info("transactionManager= {}",transactionManager);
+        log.info("jdbcTemplate= {}",jdbcTemplate);
+
+        assertThat(dataSource).isNotNull();
+        assertThat(transactionManager).isNotNull();
+        assertThat(jdbcTemplate).isNotNull();
+      //when
+
+      //then
+    }
+
+}
