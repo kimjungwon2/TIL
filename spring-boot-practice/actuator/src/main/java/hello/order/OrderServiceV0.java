0e8b2bc352b70c940629dfdc5c926a5932dc5e2c
kimjungwon2 <mac20010@naver.com>
2024-01-21 14:35:24 +0900

    FEAT: 메트릭 등록 - 예제 만들기.

---
+++ spring-boot-practice/actuator/src/main/java/hello/order/OrderServiceV0.java
@@ -0,0 +1,27 @@
+package hello.order;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OrderServiceV0 implements OrderService{
+
+    private AtomicInteger stock = new AtomicInteger(100);
+
+    @Override
+    public void order() {
+        log.info("주문");
+        stock.decrementAndGet();
+    }
+
+    @Override
+    public void cancel() {
+        log.info("취소");
+        stock.incrementAndGet();
+    }
+
+    @Override
+    public AtomicInteger getStock() {
+        return stock;
+    }
+}
