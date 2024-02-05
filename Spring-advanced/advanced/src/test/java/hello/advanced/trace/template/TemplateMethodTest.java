3a921fcc16c5c75be8511957cb2f98a1f552b93b
kimjungwon2 <mac20010@naver.com>
2024-02-05 21:33:09 +0900

    FEAT: 템플릿 메서드 패턴 - 익명 클래스.

--- Spring-advanced/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ Spring-advanced/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -49,4 +49,24 @@ public class TemplateMethodTest {
         AbstractTemplate template2 = new SubClassLogic2();
         template1.execute();
     }
+
+    @DisplayName("")
+    @Test
+    void templateMethodV2(){
+      AbstractTemplate template1 = new AbstractTemplate(){
+          @Override
+          protected void call() {
+              log.info("비즈니스 로직1 실행");
+          }
+      };
+      template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate(){
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        template2.execute();
+    }
 }
