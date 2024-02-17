f88b722910a98536a1320f28f69178018b454f86
kimjungwon2 <mac20010@naver.com>
2024-02-17 18:44:51 +0900

    FEAT: 프록시 패턴 예제 코드 - 2.

--- Spring-advanced/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ Spring-advanced/proxy/src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,5 +1,6 @@
 package hello.proxy.pureproxy.proxy;
 
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
 import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
 import hello.proxy.pureproxy.proxy.code.RealSubject;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +14,17 @@ public class ProxyPatternTest {
         RealSubject realSubject = new RealSubject();
         ProxyPatternClient client = new ProxyPatternClient(realSubject);
 
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+    @DisplayName("")
+    @Test
+    void cacheProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
         client.execute();
         client.execute();
         client.execute();
