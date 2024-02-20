6ca5e932a551ba079a028d75ff6ea636efcd6efb
kimjungwon2 <mac20010@naver.com>
2024-02-20 18:48:49 +0900

    FEAT: 데코레이터 패턴 - 예제3

--- Spring-advanced/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ Spring-advanced/proxy/src/test/java/hello/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -4,6 +4,7 @@ import hello.proxy.pureproxy.decorator.code.Component;
 import hello.proxy.pureproxy.decorator.code.DecoratorPatternClient;
 import hello.proxy.pureproxy.decorator.code.MessageDecorator;
 import hello.proxy.pureproxy.decorator.code.RealComponent;
+import hello.proxy.pureproxy.decorator.code.TimeDecorator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,4 +27,14 @@ public class DecoratorPatternTest {
         DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
         client.execute();
     }
+
+    @Test
+    void decorator2() {
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        Component timeDecorator = new TimeDecorator(messageDecorator);
+        DecoratorPatternClient client = new DecoratorPatternClient(timeDecorator);
+        client.execute();
+    }
+
 }
