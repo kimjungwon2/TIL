53ce47ff7066a54328fc541f3e32bce4e9714927
kimjungwon2 <mac20010@naver.com>
2024-02-23 20:10:00 +0900

    FEAT: 인터페이스 기반 프록시 적용.

--- Spring-advanced/proxy/src/main/java/hello/proxy/config/v1_proxy/InterfaceProxyConfig.java
+++ Spring-advanced/proxy/src/main/java/hello/proxy/config/v1_proxy/InterfaceProxyConfig.java
@@ -6,6 +6,10 @@ import hello.proxy.app.v1.OrderRepositoryV1;
 import hello.proxy.app.v1.OrderRepositoryV1Impl;
 import hello.proxy.app.v1.OrderServiceV1;
 import hello.proxy.app.v1.OrderServiceV1Impl;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderControllerInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderRepositoryInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderServiceInterfaceProxy;
+import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,18 +17,23 @@ import org.springframework.context.annotation.Configuration;
 public class InterfaceProxyConfig {
 
     @Bean
-    public OrderControllerV1 orderController(){
-        return new OrderControllerV1Impl(orderServiceV1());
+    public OrderControllerV1 orderController(LogTrace logTrace){
+        OrderControllerV1Impl controllerImpl = new OrderControllerV1Impl(orderService(logTrace));
+        return new OrderControllerInterfaceProxy(controllerImpl,logTrace);
     }
 
     @Bean
-    public OrderServiceV1 orderServiceV1(){
-        return new OrderServiceV1Impl(orderRepositoryV1());
+    public OrderServiceV1 orderService(LogTrace logTrace){
+
+        OrderServiceV1Impl serviceImpl = new OrderServiceV1Impl(orderRepository(logTrace));
+
+        return new OrderServiceInterfaceProxy(serviceImpl, logTrace);
     }
 
     @Bean
-    public OrderRepositoryV1 orderRepositoryV1(){
-        return new OrderRepositoryV1Impl();
+    public OrderRepositoryV1 orderRepository(LogTrace logTrace){
+        OrderRepositoryV1Impl repositoryImpl = new OrderRepositoryV1Impl();
+        return new OrderRepositoryInterfaceProxy(repositoryImpl,logTrace);
     }
 
 }
