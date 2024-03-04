8ff78aab0c4af06e8f349d673d9f1125fa9d0089
kimjungwon2 <mac20010@naver.com>
2024-03-04 13:26:10 +0900

    FEAT: JDK 동적 프록시 예제 2

--- Spring-advanced/proxy/src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
+++ Spring-advanced/proxy/src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -2,6 +2,8 @@ package hello.proxy.jdkdynamic;
 
 import hello.proxy.jdkdynamic.code.AImpl;
 import hello.proxy.jdkdynamic.code.AInterface;
+import hello.proxy.jdkdynamic.code.BImpl;
+import hello.proxy.jdkdynamic.code.BInterface;
 import hello.proxy.jdkdynamic.code.TimeInvocationHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,20 @@ public class JdkDynamicProxyTest {
         TimeInvocationHandler handler = new TimeInvocationHandler(target);
 
         AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+
+        proxy.call();
+        log.info("targetClass={}",target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+    }
+
+    @Test
+    void dynamicB(){
+        BInterface target = new BImpl();
+
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
 
         proxy.call();
         log.info("targetClass={}",target.getClass());
