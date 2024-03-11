417f327e942cdfe3adfc71aa160bb5905f8980f7
kimjungwon2 <mac20010@naver.com>
2024-03-11 13:39:51 +0900

    FEAT: 어드바이저

---
+++ Spring-advanced/proxy/src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -0,0 +1,29 @@
+package hello.proxy.advisor;
+
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+public class AdvisorTest {
+
+    @DisplayName("")
+    @Test
+    void advisorTest1(){
+      //given
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        //when
+        proxy.save();
+        proxy.find();
+    }
+}
