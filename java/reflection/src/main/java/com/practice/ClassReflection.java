05509790bec59779e6192677d6f9b1674327b6f9
kimjungwon2 <mac20010@naver.com>
2024-09-06 01:28:52 +0900

    TEST: Reflection의 field, Methods 물러오기

--- java/reflection/src/main/java/com/practice/ClassReflection.java
+++ java/reflection/src/main/java/com/practice/ClassReflection.java
@@ -1,11 +1,20 @@
 package com.practice;
 
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class ClassReflection {
     public void getClassReflection() throws ClassNotFoundException {
         Class<Member> aClass = Member.class;
-        Member member1 = new Member();
+        Member member1 = new Member("정원",25);
         Class<? extends Member> bClass = member1.getClass();
         Class<?> cClass = Class.forName("com.practice.Member");
+
+        Field[] fields = aClass.getFields();
+        Method[] methods = aClass.getMethods();
+        Method[] declaredMethods = aClass.getDeclaredMethods();
+        Field[] declaredFields = aClass.getDeclaredFields();
+
     }
 }
--- java/reflection/src/main/java/com/practice/Member.java
+++ java/reflection/src/main/java/com/practice/Member.java
@@ -3,4 +3,21 @@ package com.practice;
 public class Member {
     private String name;
     private int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void getName(String name){
+        this.name = name;
+    }
+
+    public void plusAge(int age){
+        this.age = this.age + age;
+    }
+
+    private void minusAge(int age){
+        this.age = this.age - age;
+    }
 }
