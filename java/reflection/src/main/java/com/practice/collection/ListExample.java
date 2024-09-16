255adae8aec996c044b6a06deffc301a4961ce49
kimjungwon2 <mac20010@naver.com>
2024-09-17 01:46:54 +0900

    고정 Array

---
+++ java/reflection/src/main/java/com/practice/collection/ListExample.java
@@ -0,0 +1,15 @@
+package com.practice.collection;
+
+public class ListExample {
+
+    public void createFixedArray(){
+        int[] arr = new int[10];
+        String[] stringArray = new String[10];
+
+        char[] charArray = {'h','e','l','l','o'};
+
+
+        System.out.println(arr[3]);
+        System.out.println(arr[arr.length-1]);
+    }
+}
