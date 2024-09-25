ae951e63995c735eb3051fec1e6b6fd78d3c64cb
kimjungwon2 <mac20010@naver.com>
2024-09-26 00:09:57 +0900

    기ã본ì ë인 list 메서드 2

--- java/reflection/src/main/java/com/practice/collection/DynamicArray.java
+++ java/reflection/src/main/java/com/practice/collection/DynamicArray.java
@@ -1,4 +1,31 @@
 package com.practice.collection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DynamicArray {
+
+    public void basicFunction(){
+        List<Integer> list = new ArrayList<>(List.of(1,2,3,4,5));
+
+        Integer i = list.get(3);
+        System.out.println(i);
+
+        list.set(4,100);
+        System.out.println(list);
+
+        list.add(100);
+        System.out.println(list);
+
+        list.add(2, 10);
+        System.out.println(list);
+
+        list.remove(list.size()-1);
+        System.out.println(list);
+
+        list.remove(2);
+
+        System.out.println(list);
+    }
+
 }
